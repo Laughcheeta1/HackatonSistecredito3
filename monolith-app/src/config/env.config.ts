0498257ac90635de726a535cf8dc27bc6af3273e
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-11-01 14:15:14 -0500

    Added a connection to MongoDb Atlas

--- monolith-app/src/config/env.config.ts
+++ monolith-app/src/config/env.config.ts
@@ -6,8 +6,11 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 // Export environment variables as simple variables
 export const PORT = process.env.PORT;
-export const DB_URL = process.env.DB_URL;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASS = process.env.DB_PASS;
 export const DB_NAME = process.env.DB_NAME;
+export const DB_USER = process.env.DB_USER;
+export const CLUSTER_NAME = process.env.CLUSTER_NAME;
+export const CLUSTER_URL = process.env.CLUSTER_URL;
+export const DB_PASS = process.env.DB_PASS;
+export const DB_RETRY_WRITES = process.env.DB_RETRY_WRITES;
+export const DB_W = process.env.DB_W;
 export const JWT_SECRET = process.env.JWT_SECRET;
--- monolith-app/src/config/typeorm.config.ts
+++ monolith-app/src/config/typeorm.config.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DB_URL } from './env.config';
+import { CLUSTER_NAME, CLUSTER_URL, DB_NAME, DB_PASS, DB_RETRY_WRITES, DB_USER, DB_W } from './env.config';
 import { Product } from 'src/product/entities/product.entity';
 import { Store } from 'src/store/entities/store.entity';
 import { StoreStock } from 'src/store-stock/entities/store-stock.entity';
@@ -10,8 +10,8 @@ import { Supplier } from 'src/supplier/entities/supplier.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: DB_URL,
-      synchronize: true, // Automatically syncs schema with DB (use with caution in production)
+      url: `mongodb+srv://${DB_USER}:${DB_PASS}@${CLUSTER_URL}/?retryWrites=${DB_RETRY_WRITES}&w=${DB_W}&appName=${CLUSTER_NAME}/${DB_NAME}`,
+      synchronize: false,
       useUnifiedTopology: true,
       entities: [
         Product, 
